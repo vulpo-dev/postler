ddf571ec99a8d2e38ac0f0dc4bb9060c16888140
Michael Riezler <michael@riezler.co>
2022-12-25 16:35:39 +0100

    feat(create-postler): resolve latest package version

--- packages/create-postler/src/main.ts
+++ packages/create-postler/src/main.ts
@@ -2,6 +2,7 @@ import { existsSync } from "fs";
 import * as fs from "fs/promises";
 import * as path from "path";
 import Mustache from "mustache";
+import axios from "axios";
 
 export async function main(templateDir: string) {
 	const projectName = process.argv.at(2);
@@ -13,7 +14,27 @@ yarn create postler <project-name>
 `);
 		return process.exit(1);
 	}
+
+	let outDir = path.join(
+		process.cwd(),
+		projectName,
+	);
+
+	if (existsSync(outDir)) {
+		console.log(`Folder ${projectName} already exists`);
+		return process.exit(2);
+	}
+
 	console.log(`Creating your project: ${projectName}`);
+
+	await fs.mkdir(outDir);
+
+	let packageJson = await generatePkgJson(projectName);
+
+	await fs.writeFile(
+		path.join(outDir, "package.json"),
+		JSON.stringify(packageJson, null, 2)
+	);
 
 	let files = await getFiles(templateDir);
 
@@ -24,6 +45,7 @@ yarn create postler <project-name>
 			projectName,
 			file.slice(templateDir.length),
 		);
+
 		let outDir = outPath.split(path.sep).slice(0, -1).join(path.sep);
 
 		if (!existsSync(outDir)) {
@@ -54,4 +76,43 @@ export async function getFiles(src: string): Promise<Array<string>> {
 	);
 
 	return sink.flat();
+}
+
+async function generatePkgJson(projectName: string) {
+	let dependencies = [
+		"@emotion/react",
+		"@emotion/styled",
+		"@types/react",
+		"postler",
+		"dotenv",
+		"react"
+	];
+
+	let resolved = await Promise.all(dependencies.map(async dep => {
+		let version = await resolveLatest(dep);
+		return [dep, version];
+	}));
+
+	return {
+		"name": projectName,
+		"private": true,
+		"version": "1.0.0",
+		"scripts": {
+			"start": "postler serve",
+			"build": "postler build"
+		},
+		"dependencies": Object.fromEntries(resolved)
+	}
+}
+
+type PackageInfo = {
+	"dist-tags": {
+		"latest": string
+	}
+}
+
+async function resolveLatest(pkgName: string) {
+	let url = `https://registry.npmjs.org/${pkgName}`
+	let parsed = await axios.get<PackageInfo>(url);
+	return parsed.data["dist-tags"].latest;
 }
